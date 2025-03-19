cd6dc16b56ca1e46c257dae531c68e7edcb99213
Jack Rogerson <cz23761@bristol.ac.uk>
2025-03-19 15:19:14 +0000

    added function to calculate the energy of the system

--- main.cpp
+++ main.cpp
@@ -15,16 +15,30 @@ int generateRandomNumber() {
     }
 }
 
+double calcEnergy(int p[]) {
+    double J = 1;
+    double E = 0;
+
+    // calculate energy of system
+    for(int i = 0; i < 99; i++) {
+        E += -J * p[i] * p[i+1];
+    }
+    cout << E << endl;
+    return E;
+}
+
 int main() {
-    int data[100];
+    int sys[100];
 
     // create rng with seed dependent on time
     srand(time(0));
 
     // populate array with 1 or -1
     for (int i = 0; i < 100; i++) {
-        data[i] = generateRandomNumber();
+        sys[i] = generateRandomNumber();
     }
+
+    calcEnergy(sys);
 
     return 0;
 }

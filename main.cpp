1efb0733c15a5873e075b60fd261c8d2b0cd5f09
Jack Rogerson <cz23761@bristol.ac.uk>
2025-03-24 14:04:10 +0000

    changed to having 1000 output files

--- main.cpp
+++ main.cpp
@@ -2,25 +2,22 @@
 #include <iostream>
 #include <ctime>
 #include <string>
-#include <chrono>
-#include <random>
-#include <ctime>
 
 using namespace std;
 
 int main() {
-    int configs = 100; // number of configurations
-    int monteCarloSteps = 10000; // number of times to pick a particle 
+    int configs = 1000; // number of configurations
+    int monteCarloSteps = 100000; // number of times to pick a particle 
 
     cout << "Running simulation..." << endl;
 
     // run the simulation
     for (int config = 0; config < configs; config++) {
         // seed the random number generator based on time iteration number
-        srand((int(time(0)) + config));
+        srand(int(time(0)) + config);
 
         // create an IsingModel object
-        IsingModel model(1, 0.01, 100, 100); // J, beta, rows, cols
+        IsingModel model(1, 0.2, 100, 100); // J, beta, rows, cols
         
         model.runSimulation(monteCarloSteps);
 
